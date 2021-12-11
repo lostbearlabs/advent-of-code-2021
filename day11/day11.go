08161b6c37a29d6b8709319a3239baeb2999bb17
eric.johnson <eric.johnson@code42.com>
2021-12-11 11:17:20 -0600

    day11

--- day11/day11.go
+++ day11/day11.go
@@ -1,10 +1,114 @@
 package main
 
+import (
+	"aoc21/lib"
+	"log"
+	"os"
+)
+
 // Usage:
 //   go run . < input
 
 // https://adventofcode.com/2021/day/11
 
 func main() {
+	ar := lib.ReadDigitsArray(os.Stdin)
+
+	count := CountFlashes(ar, 100)
+	log.Println("count at 100:", count)
+
+	all := AllFlash(ar)
+	log.Println("all flash after an additional: ", all)
 }
 
+func AllFlash(ar [][]int) int {
+	n := 1
+	for {
+		Increment(ar)
+		Propagate(ar)
+		flashes := CountAndReset(ar)
+		if flashes == len(ar)*len(ar) {
+			return n
+		}
+		n++
+	}
+}
+
+func CountFlashes(ar [][]int, numSteps int) int {
+
+	flashes := 0
+	for i := 0; i < numSteps; i++ {
+		Increment(ar)
+		Propagate(ar)
+		n := CountAndReset(ar)
+		flashes += n
+	}
+	return flashes
+
+}
+
+func Increment(ar [][]int) {
+	for i := 0; i < len(ar); i++ {
+		for j := 0; j < len(ar[i]); j++ {
+			ar[i][j]++
+		}
+	}
+}
+
+// CountAndReset
+// at end of each turn, count cells that have flashed and set them to zero
+func CountAndReset(ar [][]int) int {
+	count := 0
+	for i := 0; i < len(ar); i++ {
+		for j := 0; j < len(ar[i]); j++ {
+			if ar[i][j] > 9 {
+				ar[i][j] = 0
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func Propagate(ar [][]int) {
+
+	for i := 0; i < len(ar); i++ {
+		for j := 0; j < len(ar[i]); j++ {
+			spreadFrom(ar, i, j, false, " ")
+		}
+	}
+}
+
+func spreadFrom(ar [][]int, i int, j int, incr bool, pad string) {
+
+	// not in the grid
+	if i < 0 || j < 0 || i >= len(ar) || j >= len(ar[i]) {
+		return
+	}
+
+	//log.Println(pad, "visit i=", i, ", j=", j, ", val", ar[i][j])
+
+	if ar[i][j] == 999 {
+		return // already flashed
+	}
+
+	if incr {
+		//log.Println(pad, "incr i=", i, ", j=", j)
+		ar[i][j]++
+	}
+
+	if ar[i][j] <= 9 {
+		return // not ready to flash
+	}
+
+	//log.Println(pad, "flash! i=", i, ", j=", j)
+	ar[i][j] = 999 // flash now
+
+	for di := -1; di <= 1; di++ {
+		for dj := -1; dj <= 1; dj++ {
+			if di != 0 || dj != 0 {
+				spreadFrom(ar, i+di, j+dj, true, pad+"  ")
+			}
+		}
+	}
+}
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -1,2 +1,70 @@
 package main
 
+import (
+	"aoc21/lib"
+	"strings"
+	"testing"
+)
+
+func TestSmallExample(t *testing.T) {
+	input := `11111
+19991
+19191
+19991
+11111`
+	reader := strings.NewReader(input)
+	ar := lib.ReadDigitsArray(reader)
+	lib.AssertEq(t, "after 1", 9, CountFlashes(ar, 1))
+}
+
+func TestBigExample10(t *testing.T) {
+	input := `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
+
+	reader := strings.NewReader(input)
+	ar := lib.ReadDigitsArray(reader)
+	lib.AssertEq(t, "after 10", 204, CountFlashes(ar, 10))
+	//lib.AssertEq(t, "after 100", 1656, CountFlashes(ar, 100))
+}
+
+func TestBigExample100(t *testing.T) {
+	input := `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
+
+	reader := strings.NewReader(input)
+	ar := lib.ReadDigitsArray(reader)
+	lib.AssertEq(t, "after 100", 1656, CountFlashes(ar, 100))
+}
+
+func TestBigExampleAll(t *testing.T) {
+	input := `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
+
+	reader := strings.NewReader(input)
+	ar := lib.ReadDigitsArray(reader)
+	lib.AssertEq(t, "after 100", 195, AllFlash(ar))
+}
