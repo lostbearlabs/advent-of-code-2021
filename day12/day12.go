0aaac298edd57bbc76b28b7c95b4047a29406c9b
eric.johnson <eric.johnson@code42.com>
2021-12-12 09:41:41 -0600

    day12

--- day12/day12.go
+++ day12/day12.go
@@ -1,9 +1,95 @@
 package main
 
+import (
+	"aoc21/lib"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
+
 // Usage:
 //   go run . < input
 
-// https://adventofcode.com/2021/day/11
+// https://adventofcode.com/2021/day/12
 
 func main() {
+	lines := lib.ReadLines(os.Stdin)
+	graph := ParseGraph(lines)
+
+	count := CountPaths(graph)
+	log.Println("count: ", count)
+
+	count2 := CountPaths2(graph)
+	log.Println("count2: ", count2)
+}
+
+func ParseGraph(lines []string) map[string][]string {
+	graph := make(map[string][]string)
+	for _, line := range lines {
+		ar := strings.Split(line, "-")
+		graph[ar[0]] = append(graph[ar[0]], ar[1])
+		graph[ar[1]] = append(graph[ar[1]], ar[0])
+	}
+	return graph
+}
+
+type State struct {
+	visited      map[string]bool
+	allowRevisit bool
+	revisited    string
+}
+
+func NewState() State {
+	return State{
+		visited: make(map[string]bool),
+	}
+}
+
+func CountPaths(graph map[string][]string) int {
+	state := NewState()
+	return CountPathsFrom(graph, "start", state)
+}
+
+func CountPaths2(graph map[string][]string) int {
+	state := NewState()
+	state.allowRevisit = true
+	return CountPathsFrom(graph, "start", state)
+}
+
+func CountPathsFrom(graph map[string][]string, node string, state State) int {
+	n := 0
+
+	//log.Println(pad, node)
+	if node == "end" {
+		return 1
+	}
+
+	if unicode.IsLower(rune(node[0])) {
+		if state.visited[node] {
+			if state.allowRevisit {
+				if state.revisited != "" || node == "start" || node == "end" {
+					return 0
+				} else {
+					state.revisited = node
+				}
+			} else {
+				return 0
+			}
+		} else {
+			state.visited[node] = true
+		}
+	}
+
+	for _, next := range graph[node] {
+		n += CountPathsFrom(graph, next, state)
+	}
+
+	if state.revisited == node {
+		state.revisited = ""
+	} else {
+		state.visited[node] = false
+	}
+
+	return n
 }
--- day12/day12_test.go
+++ day12/day12_test.go
@@ -1,1 +1,79 @@
 package main
+
+import (
+	"aoc21/lib"
+	"strings"
+	"testing"
+)
+
+func TestExample1(t *testing.T) {
+	input := `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`
+
+	rd := strings.NewReader(input)
+	lines := lib.ReadLines(rd)
+	graph := ParseGraph(lines)
+
+	count := CountPaths(graph)
+	lib.AssertEq(t, "count", 10, count)
+
+	count2 := CountPaths2(graph)
+	lib.AssertEq(t, "count2", 36, count2)
+}
+
+func TestExample2(t *testing.T) {
+	input := `dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc`
+
+	rd := strings.NewReader(input)
+	lines := lib.ReadLines(rd)
+	graph := ParseGraph(lines)
+	count := CountPaths(graph)
+	lib.AssertEq(t, "count", 19, count)
+
+	count2 := CountPaths2(graph)
+	lib.AssertEq(t, "count2", 103, count2)
+}
+
+func TestExample3(t *testing.T) {
+	input := `fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW`
+
+	rd := strings.NewReader(input)
+	lines := lib.ReadLines(rd)
+	graph := ParseGraph(lines)
+	count := CountPaths(graph)
+	lib.AssertEq(t, "count", 226, count)
+
+	count2 := CountPaths2(graph)
+	lib.AssertEq(t, "count2", 3509, count2)
+}
