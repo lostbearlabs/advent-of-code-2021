aa4c263a1ec200b4d9aaba1c004a6a7085c7426f
eric.johnson <eric.johnson@code42.com>
2021-12-13 10:00:12 -0600

    day13

--- day13/day13.go
+++ day13/day13.go
@@ -1,9 +1,166 @@
 package main
 
+import (
+	"aoc21/lib"
+	"fmt"
+	"image"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 // Usage:
 //   go run . < input
 
-// https://adventofcode.com/2021/day/11
+// https://adventofcode.com/2021/day/13
 
 func main() {
+	fnord := ReadFnord(os.Stdin)
+
+	mp := fnord.points
+	for i, in := range fnord.instructions {
+		if i == 0 {
+			mx, my := MaxXY(mp)
+			log.Println("init count ", len(mp), ", max-x-y ", mx, my)
+			log.Println()
+		}
+		mp = DoFold(mp, in)
+		mx, my := MaxXY(mp)
+		log.Println("fold ", i, ", count ", len(mp), ", max-x-y ", mx, my)
+		log.Println()
+	}
+
+	Display(mp)
+}
+
+type Instruction struct {
+	coord    int
+	isCoordX bool
+}
+
+type Fnord struct {
+	// points on the page
+	points map[image.Point]bool
+
+	// list of fold line, true=x or false=y
+	instructions []Instruction
+}
+
+func NewFnord() Fnord {
+	return Fnord{
+		points:       make(map[image.Point]bool),
+		instructions: nil,
+	}
+}
+
+func ReadFnord(rd io.Reader) Fnord {
+	lines := lib.ReadLines(rd)
+	part1 := true
+	fnord := NewFnord()
+
+	for _, line := range lines {
+		if line == "" {
+			part1 = false
+		} else if part1 {
+			ar := strings.Split(line, ",")
+			x, _ := strconv.Atoi(ar[0])
+			y, _ := strconv.Atoi(ar[1])
+			pt := image.Point{
+				X: x,
+				Y: y,
+			}
+			fnord.points[pt] = true
+		} else {
+			ar := strings.Split(line, "=")
+			isX := ar[0] == "fold along x"
+			c, _ := strconv.Atoi(ar[1])
+			in := Instruction{
+				coord:    c,
+				isCoordX: isX,
+			}
+			fnord.instructions = append(fnord.instructions, in)
+		}
+	}
+
+	return fnord
+}
+
+func DoFold(points map[image.Point]bool, in Instruction) map[image.Point]bool {
+
+	if in.isCoordX {
+		log.Println("Fold along x=", in.coord)
+	} else {
+		log.Println("Fold along y=", in.coord)
+	}
+
+	after := make(map[image.Point]bool)
+	for pt, _ := range points {
+		qt := Fold(pt, in)
+		after[qt] = true
+	}
+	return after
+}
+
+func Fold(pt image.Point, in Instruction) image.Point {
+	if in.isCoordX {
+		if pt.X > in.coord {
+			return image.Point{
+				X: 2*in.coord - pt.X,
+				Y: pt.Y,
+			}
+		} else {
+			return pt
+		}
+	} else {
+		if pt.Y > in.coord {
+			return image.Point{
+				X: pt.X,
+				Y: 2*in.coord - pt.Y,
+			}
+		} else {
+			return pt
+		}
+	}
+}
+
+func MaxXY(points map[image.Point]bool) (int, int) {
+
+	maxX := 0
+	maxY := 0
+	for pt, _ := range points {
+		if pt.X > maxX {
+			maxX = pt.X
+		}
+		if pt.Y > maxY {
+			maxY = pt.Y
+		}
+	}
+
+	return maxX, maxY
+}
+
+func Display(points map[image.Point]bool) {
+
+	fmt.Println()
+
+	maxX, maxY := MaxXY(points)
+
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x <= maxX; x++ {
+			qt := image.Point{
+				X: x,
+				Y: y,
+			}
+			if points[qt] {
+				os.Stdout.WriteString("*")
+			} else {
+				os.Stdout.WriteString(" ")
+			}
+		}
+		os.Stdout.WriteString("\n")
+	}
+
+	fmt.Println()
 }
--- day13/day13_test.go
+++ day13/day13_test.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"aoc21/lib"
+	"strings"
+	"testing"
+)
+
+func sampleInput() string {
+	return `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`
+}
+
+func TestSampleOneFold(t *testing.T) {
+	rd := strings.NewReader(sampleInput())
+	fnord := ReadFnord(rd)
+	mp := DoFold(fnord.points, fnord.instructions[0])
+	lib.AssertEq(t, "num points", 17, len(mp))
+}
+
+func TestSampleAllFolds(t *testing.T) {
+	rd := strings.NewReader(sampleInput())
+	fnord := ReadFnord(rd)
+
+	Display(fnord.points)
+
+	mp := DoFold(fnord.points, fnord.instructions[0])
+	lib.AssertEq(t, "num points", 17, len(mp))
+	Display(mp)
+
+	mp = DoFold(mp, fnord.instructions[1])
+	Display(mp)
+
+	mp = DoFold(mp, fnord.instructions[1])
+	Display(mp)
+
+}
