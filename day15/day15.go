7802992b7f182e80dffd3c5244d91e66cc6c54e0
eric.johnson <eric.johnson@code42.com>
2021-12-15 12:25:53 -0600

    day15

--- day15/day15.go
+++ day15/day15.go
@@ -1,9 +1,148 @@
 package main
 
+import (
+	"aoc21/lib"
+	"image"
+	"log"
+	"os"
+)
+
 // Usage:
 //   go run . < input
 
-// https://adventofcode.com/2021/day/11
+// https://adventofcode.com/2021/day/15
+
+const max int = 1_000_000_000
 
 func main() {
+	grid := lib.ReadDigitsArray(os.Stdin)
+	cost := ShortestCornerToCornerCost(grid)
+	log.Println("Cost: ", cost)
+
+	expanded := Expand(grid)
+	cost = ShortestCornerToCornerCost(expanded)
+	log.Println("Expanded: ", cost)
+}
+
+func ShortestCornerToCornerCost(grid [][]int) int {
+	// initial path is to starting point with cost zero
+	pt := image.Pt(0, 0)
+	pathSet := PathSet{
+		paths: map[image.Point]Path{pt: NewPath(pt, 0)},
+		todo:  map[image.Point]Path{pt: NewPath(pt, 0)},
+	}
+
+	// we're trying to get to this point
+	tgt := image.Pt(len(grid)-1, len(grid[0])-1)
+
+	// here we go ...
+	return search(grid, tgt, pathSet)
+}
+
+// This is just Dijkstra ...
+func search(grid [][]int, tgt image.Point, pathSet PathSet) int {
+
+	for {
+		// have we reached our target?  if so, done!
+		probe, ok := pathSet.paths[tgt]
+		if ok {
+			log.Println("found ", probe)
+			return probe.cost
+		}
+
+		deltas := []image.Point{
+			{X: 1, Y: 0},
+			{X: 0, Y: 1},
+			{X: -1, Y: 0},
+			{X: 0, Y: -1},
+		}
+
+		path := cheapestTodo(&pathSet)
+		//log.Println("next: ", path)
+
+		for _, delta := range deltas {
+			addIfPossible(grid, path, delta, &pathSet)
+		}
+
+		delete(pathSet.todo, path.pt)
+	}
+}
+
+func addIfPossible(grid [][]int, path Path, delta image.Point, pathSet *PathSet) {
+
+	x := path.pt.X + delta.X
+	y := path.pt.Y + delta.Y
+	qt := image.Pt(x, y)
+
+	// illegal point
+	if x < 0 || y < 0 || x >= len(grid) || y >= len(grid[0]) {
+		return
+	}
+
+	// already processed
+	_, ok := pathSet.paths[qt]
+	if ok {
+		return
+	}
+
+	newPath := NewPath(qt, path.cost+grid[x][y])
+	//log.Println("add ", newPath)
+	pathSet.paths[newPath.pt] = newPath
+	pathSet.todo[newPath.pt] = newPath
+}
+
+func cheapestTodo(pathSet *PathSet) Path {
+	var best Path
+	first := true
+	for _, path := range pathSet.todo {
+		if first || path.cost < best.cost {
+			best = path
+			first = false
+		}
+	}
+	if first {
+		log.Fatal("never found cheapest")
+	}
+
+	return best
+}
+
+// *****************
+// Path/Pathset - for keeping track of the frontier
+// *****************
+
+type PathSet struct {
+	paths map[image.Point]Path
+	todo  map[image.Point]Path
+}
+
+type Path struct {
+	pt   image.Point
+	cost int
+}
+
+func NewPath(pt image.Point, cost int) Path {
+	return Path{pt: pt, cost: cost}
+}
+
+func Expand(grid [][]int) [][]int {
+	n := 5
+	d := make([][]int, n*len(grid))
+	for i := 0; i < len(d); i++ {
+		d[i] = make([]int, n*len(grid))
+	}
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			for di := 0; di < n; di++ {
+				for dj := 0; dj < n; dj++ {
+					ii := i + di*len(grid)
+					jj := j + dj*(len(grid[0]))
+					d[ii][jj] = (grid[i][j]-1+di+dj)%9 + 1
+				}
+			}
+		}
+	}
+
+	return d
 }
--- day15/day15_test.go
+++ day15/day15_test.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"aoc21/lib"
+	"strings"
+	"testing"
+)
+
+func sample() string {
+	return `1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581`
+}
+
+func TestSample(t *testing.T) {
+	rd := strings.NewReader(sample())
+	grid := lib.ReadDigitsArray(rd)
+	cost := ShortestCornerToCornerCost(grid)
+	lib.AssertEq(t, "cost", 40, cost)
+}
+
+func TestExpandedSample(t *testing.T) {
+	rd := strings.NewReader(sample())
+	grid := lib.ReadDigitsArray(rd)
+	grid = Expand(grid)
+	cost := ShortestCornerToCornerCost(grid)
+	lib.AssertEq(t, "cost", 315, cost)
+}
