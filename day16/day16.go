e9f730662dabcf4371144f914534eaf9e209b8af
eric.johnson <eric.johnson@code42.com>
2021-12-16 16:29:26 -0600

    day16

--- day16/day16.go
+++ day16/day16.go
@@ -1,9 +1,206 @@
 package main
 
+import (
+	"aoc21/lib"
+	"log"
+	"os"
+)
+
 // Usage:
 //   go run . < input
 
 // https://adventofcode.com/2021/day/11
 
 func main() {
+	lines := lib.ReadLines(os.Stdin)
+	bits := ParseHex(lines[0])
+	packet := ParsePacket(bits, 0)
+	sum := SumVersions(packet)
+	log.Println("sum of versions: ", sum)
+
+	val := ComputeExpression(packet)
+	log.Println("result of message: ", val)
+}
+
+func ParseHex(st string) []uint8 {
+	ar := make([]uint8, 4*len(st))
+
+	for i, c := range st {
+		var d uint8
+		if '0' <= c && c <= '9' {
+			d = uint8(c - '0')
+		} else {
+			d = uint8(10 + c - 'A')
+		}
+
+		for j := 0; j < 4; j++ {
+			ar[i*4+3-j] = d >> j & 1
+		}
+	}
+
+	return ar
+}
+
+func BinaryToText(bits []uint8) string {
+	st := ""
+	for _, b := range bits {
+		if b > 0 {
+			st = st + "1"
+		} else {
+			st = st + "0"
+		}
+	}
+	return st
+}
+
+const (
+	LITERAL = iota
+)
+
+type Packet struct {
+	packetVersion int
+	packetType    int
+	packetLiteral int
+	packetLength  int
+	subPackets    []Packet
+}
+
+func ParsePacket(bits []uint8, offset int) Packet {
+	subPackets := []Packet{}
+
+	pos := offset
+	packetVersion := ReadInt(bits, pos, 3)
+	pos += 3
+	packetType := ReadInt(bits, pos, 3)
+	pos += 3
+
+	packetLiteral := 0
+
+	if packetType == 4 {
+		lastPacket := false
+		for !lastPacket {
+			if bits[pos] == 0 {
+				lastPacket = true
+			}
+			pos += 1
+			packetLiteral = packetLiteral<<4 + ReadInt(bits, pos, 4)
+			log.Println("pos", pos, "literal", packetLiteral)
+			pos += 4
+		}
+	} else {
+		lengthTypeId := bits[pos]
+		pos += 1
+
+		if lengthTypeId == 0 {
+			totalSubPacketLength := ReadInt(bits, pos, 15)
+			pos += 15
+			log.Println("totalSubPacketLength", totalSubPacketLength)
+			for n := 0; n < totalSubPacketLength; {
+				sub := ParsePacket(bits, pos)
+				pos += sub.packetLength
+				n += sub.packetLength
+				log.Println(" read subpacket, n=", n)
+				subPackets = append(subPackets, sub)
+			}
+		} else {
+			totalNumSubPackets := ReadInt(bits, pos, 11)
+			pos += 11
+			for i := 0; i < totalNumSubPackets; i++ {
+				sub := ParsePacket(bits, pos)
+				pos += sub.packetLength
+				subPackets = append(subPackets, sub)
+			}
+		}
+	}
+
+	//// increment to next hex digit boundary
+	//for pos%4 > 0 {
+	//	pos++
+	//}
+
+	return Packet{
+		packetVersion: packetVersion,
+		packetType:    packetType,
+		packetLiteral: packetLiteral,
+		packetLength:  pos - offset,
+		subPackets:    subPackets,
+	}
+
+}
+
+func ReadInt(bits []uint8, offset int, numBits int) int {
+	val := 0
+	for i := 0; i < numBits; i++ {
+		val = val<<1 + int(bits[offset+i])
+	}
+	return val
+}
+
+func SumVersions(packet Packet) int {
+	sum := packet.packetVersion
+	for _, q := range packet.subPackets {
+		sum += SumVersions(q)
+	}
+	return sum
+}
+
+func ComputeExpression(packet Packet) int {
+
+	ar := make([]int, len(packet.subPackets))
+	for i, q := range packet.subPackets {
+		ar[i] = ComputeExpression(q)
+	}
+
+	switch packet.packetType {
+	case 0: // sum
+		return fold(0, ar, func(i, j int) int { return i + j })
+	case 1: // product
+		return fold(1, ar, func(i, j int) int { return i * j })
+	case 2: // minimum
+		return fold(1_000_000_000, ar, func(i, j int) int {
+			if i < j {
+				return i
+			} else {
+				return j
+			}
+		})
+	case 3: // maximum
+		return fold(-1_000_000_000, ar, func(i, j int) int {
+			if i > j {
+				return i
+			} else {
+				return j
+			}
+		})
+	case 4: // literal
+		return packet.packetLiteral
+	case 5: // gt
+		if ar[0] > ar[1] {
+			return 1
+		} else {
+			return 0
+		}
+	case 6: // lt
+		if ar[0] < ar[1] {
+			return 1
+		} else {
+			return 0
+		}
+	case 7: // eq
+		if ar[0] == ar[1] {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	log.Fatal("bad packetType ", packet.packetType)
+	return 0
+}
+
+func fold(init int, ar []int, fn func(int, int) int) int {
+	x := init
+	for _, n := range ar {
+		x = fn(x, n)
+	}
+	return x
 }
--- day16/day16_test.go
+++ day16/day16_test.go
@@ -1,1 +1,72 @@
 package main
+
+import (
+	"aoc21/lib"
+	"testing"
+)
+
+func TestParseHex(t *testing.T) {
+	lib.AssertEqStr(t, "5", "0101", BinaryToText(ParseHex("5")))
+	lib.AssertEqStr(t, "A", "1010", BinaryToText(ParseHex("A")))
+	lib.AssertEqStr(t, "F", "1111", BinaryToText(ParseHex("F")))
+
+	text := "38006F45291200"
+	expectedBinaryText := "00111000000000000110111101000101001010010001001000000000"
+	binary := ParseHex(text)
+	binaryText := BinaryToText(binary)
+	lib.AssertEqStr(t, "", expectedBinaryText, binaryText)
+}
+
+func TestReadInt(t *testing.T) {
+	bits := []uint8{1, 0, 1}
+	lib.AssertEq(t, "3 bits", 5, ReadInt(bits, 0, 3))
+}
+
+func TestParsePacket(t *testing.T) {
+	bits := ParseHex("D2FE28")
+	packet := ParsePacket(bits, 0)
+	lib.AssertEq(t, "packetVersion", 6, packet.packetVersion)
+	lib.AssertEq(t, "packetType", 4, packet.packetType)
+	lib.AssertEq(t, "packetLiteral", 2021, packet.packetLiteral)
+}
+
+func TestParseSubPacketType0(t *testing.T) {
+	bits := ParseHex("38006F45291200")
+	packet := ParsePacket(bits, 0)
+	lib.AssertEq(t, "packetVersion", 1, packet.packetVersion)
+	lib.AssertEq(t, "packetType", 6, packet.packetType)
+	lib.AssertEq(t, "packetLiteral", 0, packet.packetLiteral)
+	lib.AssertEq(t, "len(sub)", 2, len(packet.subPackets))
+	lib.AssertEq(t, "sub[0].packetLiteral", 10, packet.subPackets[0].packetLiteral)
+	lib.AssertEq(t, "sub[1].packetLiteral", 20, packet.subPackets[1].packetLiteral)
+}
+
+func TestParseSubPacketType1(t *testing.T) {
+	bits := ParseHex("EE00D40C823060")
+	packet := ParsePacket(bits, 0)
+	lib.AssertEq(t, "packetVersion", 7, packet.packetVersion)
+	lib.AssertEq(t, "packetType", 3, packet.packetType)
+	lib.AssertEq(t, "packetLiteral", 0, packet.packetLiteral)
+	lib.AssertEq(t, "len(sub)", 3, len(packet.subPackets))
+	lib.AssertEq(t, "sub[0].packetLiteral", 1, packet.subPackets[0].packetLiteral)
+	lib.AssertEq(t, "sub[1].packetLiteral", 2, packet.subPackets[1].packetLiteral)
+	lib.AssertEq(t, "sub[2].packetLiteral", 3, packet.subPackets[2].packetLiteral)
+}
+
+func TestSumVersions(t *testing.T) {
+	lib.AssertEq(t, "packetVersion1", 16, SumVersions(ParsePacket(ParseHex("8A004A801A8002F478"), 0)))
+	lib.AssertEq(t, "packetVersion2", 12, SumVersions(ParsePacket(ParseHex("620080001611562C8802118E34"), 0)))
+	lib.AssertEq(t, "packetVersion3", 23, SumVersions(ParsePacket(ParseHex("C0015000016115A2E0802F182340"), 0)))
+	lib.AssertEq(t, "packetVersion4", 31, SumVersions(ParsePacket(ParseHex("A0016C880162017C3686B18A3D4780"), 0)))
+}
+
+func TestComputeExpression(t *testing.T) {
+	lib.AssertEq(t, "1", 3, ComputeExpression(ParsePacket(ParseHex("C200B40A82"), 0)))
+	lib.AssertEq(t, "2", 54, ComputeExpression(ParsePacket(ParseHex("04005AC33890"), 0)))
+	lib.AssertEq(t, "3", 7, ComputeExpression(ParsePacket(ParseHex("880086C3E88112"), 0)))
+	lib.AssertEq(t, "4", 9, ComputeExpression(ParsePacket(ParseHex("CE00C43D881120"), 0)))
+	lib.AssertEq(t, "5", 1, ComputeExpression(ParsePacket(ParseHex("D8005AC2A8F0"), 0)))
+	lib.AssertEq(t, "6", 0, ComputeExpression(ParsePacket(ParseHex("F600BC2D8F"), 0)))
+	lib.AssertEq(t, "7", 0, ComputeExpression(ParsePacket(ParseHex("9C005AC2F8F0"), 0)))
+	lib.AssertEq(t, "8", 1, ComputeExpression(ParsePacket(ParseHex("9C0141080250320F1802104A08"), 0)))
+}
